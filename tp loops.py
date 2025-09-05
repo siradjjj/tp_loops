3f84546b5b5fabda89a8e44872496a7827a870e1
Siradj <sifisiradj7@gmail.com>
2025-09-04 23:42:36 -0700

    First commit

---
+++ tp loops.py
@@ -0,0 +1,25 @@
+for i in range(1, 51):
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(i)
+
+
+
+
+
+
+while True:
+    guess = int(input("Guess the secret number: "))
+    
+    if guess > secret_number:
+        print("Too high! Try again.")
+    elif guess < secret_number:
+        print("Too low! Try again.")
+    else:
+        print("Congratulations! You've guessed it.")
+        break
